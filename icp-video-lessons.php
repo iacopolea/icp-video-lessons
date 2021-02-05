d4b64c97a021dd3ef87df54f7adad13194a1575d
iacopolea <iacopo.lea@gmail.com>
2021-02-05 15:50:11 +0100

    wp scaffold plugin

---
+++ icp-video-lessons.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Icp Video Lessons
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     icp-video-lessons
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Icp_Video_Lessons
+ */
+
+// Your code starts here.
